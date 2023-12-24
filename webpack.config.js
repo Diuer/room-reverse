1b7480b7b32c62ad8c45c61408e3bf97a933881e
Diuer <diuer71@gmail.com>
2023-12-24 23:36:28 +0800

    chore: fix run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,34 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  mode: "development",
-  entry: [
-    "webpack-dev-server/client?http://localhost:8080",
-    "webpack/hot/dev-server",
-    "./index",
-  ],
-  output: {
-    path: path.resolve(__dirname, "build"), // output folder
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader", // for styles
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html", // base html
-    }),
-  ],
+	mode: "development",
+	entry: ["./index"],
+	output: {
+		path: path.resolve(__dirname, "build"), // output folder
+		publicPath: "./",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html", // base html
+		}),
+	],
 };
